00c67e6048cc91043b3225346011978f6aea4989
Alan Falloon <alan.falloon@gmail.com>
2010-04-02 01:07:33 -0400

    Add command line options to show help, show version, and set entropy

--- Main.hs
+++ Main.hs
@@ -1,14 +1,42 @@
 module Main where
 
 import Data.Array.IArray
-import ERandom
+import ERandom hiding (entropy)
+import System.Console.GetOpt
+import System.Environment
+import System.Exit
 import System.Random
 import Words
 
+header = "Usage: wordjunk [options]"
+
+options :: [OptDescr (Options -> IO Options)]
+options =
+    [ Option ['V'] ["version"] (NoArg showVersion) "show the current version and exit."
+    , Option ['h'] ["help"]    (NoArg showHelp)    "Show this help and exit."
+    , Option ['e'] ["entropy"] (ReqArg setEntropy "BITS") entropyHelp
+    ]
+    where
+      entropyHelp = "The minimum amount of entropy in the generated" ++
+                    " word. Entropy is measured in bits, more bits means" ++
+                    " a longer word, but stronger password. The default" ++
+                    " value is 128 bits, which is enough for almost" ++
+                    " anyone"
+
+data Options = Options {
+      entropy :: Entropy
+    }
+
 main :: IO ()
 main = do
+  args <- getArgs
+  flags <- case getOpt RequireOrder options args of
+            (o, [], []) -> return o
+            (_, fl, []) -> error $ "unrecognized arguments: " ++ unwords fl
+            (_, _ , m ) -> error $ concat m ++ usageInfo header options
+  opts <- foldl (>>=) (return defaultOptions) flags
   ws <- loadWords
-  junk <- runERandomIO (makeJunk 128 ws)
+  junk <- runERandomIO (makeJunk (entropy opts) ws)
   putStrLn junk
 
 makeJunk :: (Random i, Ix i, RandomGen g) =>
@@ -41,3 +69,33 @@ randomizeWord (c:w) = do
   return (c':w')
     where
       alts = l33tLetters ! c
+
+showVersion :: Options -> IO Options
+showVersion _ = do
+  putStrLn ver
+  exitWith ExitSuccess
+      where
+        ver = unlines
+                [ "wordjunk 0.1"
+                , "Copyright (C) 2010 Alan Falloon <alan.falloon@gmail.com>"
+                , "This program is free software; " ++
+                  "you may redistribute it under the terms of"
+                , "the GNU General Public License version 3 or" ++
+                  " (at your option) a later version."
+                , "This program has absolutely no warranty."
+                ]
+
+showHelp :: Options -> IO Options
+showHelp _ = do
+  putStrLn $ usageInfo header options
+  exitWith ExitSuccess
+
+setEntropy :: String -> Options -> IO Options
+setEntropy entStr opt = do
+  case reads entStr of
+    [(ent,"")] -> return (opt { entropy = ent })
+    _          -> error $ "entropy must be a number, not " ++ entStr
+
+defaultOptions = Options {
+                   entropy = 128
+                 }
